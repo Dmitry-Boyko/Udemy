9dad1f8fa198de7293cfa44c6be747b67df056a1
Dmitry <dmt.b@yahoo.com>
2023-01-09 16:42:46 -0500

    102

--- Udemy-FullStack01/Section 12. Class-Based Components/hemisphere/src/hemisphereDisplay.js
+++ Udemy-FullStack01/Section 12. Class-Based Components/hemisphere/src/hemisphereDisplay.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import northPic from './images/north.jpg';
 import southPic from './images/south.jpg';
+import './hemisphere.css';
+
+const hemisphereConfig = {
+    Northern: {
+        text: 'it is northern hemisphere',
+        picture: northPic
+    },
+    Southern: {
+        text: 'it is southern hemisphere',
+        picture: southPic
+    }
+}
 
 const HemisphereDisplay = ({ latitude }) => {
 
-    const hemisphere = latitude > 0 ? 'North hemisphere' : 'South hemisphere';
-    const picture = latitude > 0 ? northPic : southPic;
+    const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
+    const {text, picture} = hemisphereConfig[hemisphere]
 
     return(
-        <div src={picture} alt="" >
-            { hemisphere }
+        <div className = {hemisphere}>
+            <div className = 'ui raised text container segmant'>
+                <div className = 'image'>
+                <img src={picture} alt="hemisphere picture" />
+                </div>
+                <div className = 'ui teal bottom attached label'>
+                    <hi> {text} </hi>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default HemisphereDisplay;
-
