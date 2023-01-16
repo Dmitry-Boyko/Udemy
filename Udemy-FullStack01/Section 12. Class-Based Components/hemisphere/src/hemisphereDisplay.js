3cf01f3d2b47b9d77a004c1acb1d8ce636fda7c8
Dmitry <dmt.b@yahoo.com>
2023-01-16 15:59:31 -0500

    section12 done

--- Udemy-FullStack01/Section 12. Class-Based Components/hemisphere/src/hemisphereDisplay.js
+++ Udemy-FullStack01/Section 12. Class-Based Components/hemisphere/src/hemisphereDisplay.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import northPic from './images/north.jpg';
-import southPic from './images/south.jpg';
-import './hemisphere.css';
+import React from "react";
+import northPic from "./images/north.jpg";
+import southPic from "./images/south.jpg";
+import "./hemisphere.css";
 
 const hemisphereConfig = {
-    Northern: {
-        text: 'it is northern hemisphere',
-        picture: northPic
-    },
-    Southern: {
-        text: 'it is southern hemisphere',
-        picture: southPic
-    }
-}
+  Northern: {
+    text: "it is northern hemisphere",
+    picture: northPic,
+  },
+  Southern: {
+    text: "it is southern hemisphere",
+    picture: southPic,
+  },
+};
 
 const HemisphereDisplay = ({ latitude }) => {
+  const hemisphere = latitude > 0 ? "Northern" : "Southern";
+  const { text, picture } = hemisphereConfig[hemisphere];
 
-    const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
-    const {text, picture} = hemisphereConfig[hemisphere]
-
-    return(
-        <div className = {hemisphere}>
-            <div className = 'ui raised text container segmant'>
-                <div className = 'image'>
-                <img src={picture} alt="hemisphere picture" />
-                </div>
-                <div className = 'ui teal bottom attached label'>
-                    <hi> {text} </hi>
-                </div>
-            </div>
+  return (
+    <div className={hemisphere}>
+      <div className="ui raised text container segmant">
+        <div className="image">
+          <img src={picture} alt="hemisphere picture" />
         </div>
-    )
-}
+        <div className="ui teal bottom attached label">
+          <hi>{text}</hi>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default HemisphereDisplay;
