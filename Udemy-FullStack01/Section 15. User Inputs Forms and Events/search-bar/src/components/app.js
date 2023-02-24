f0032907495287ad9631b1f6d46a7d6633aa8de2
Dmitry-Boyko <dmt.b@yahoo.com>
2023-02-24 11:26:15 -0500

    images-on-the-page

--- Udemy-FullStack01/Section 15. User Inputs Forms and Events/search-bar/src/components/app.js
+++ Udemy-FullStack01/Section 15. User Inputs Forms and Events/search-bar/src/components/app.js
@@ -1,21 +1,30 @@
 import React from "react";
 import SearchInput from './searchInput';
+import axios from 'axios';
+import ImageList from './imageList';
 
 class App extends React.Component{
+
   constructor(props){
     super(props)
+
+    this.state = { images: [] }
 
     this.onSearchSubmit = this.onSearchSubmit.bind(this)
   }
 
-  onSearchSubmit(entry){
-    console.log(entry)
+  async onSearchSubmit(entry){
+    const response = await axios.get(`https://pixabay.com/api/?key=33781033-801561e0de274eca44780a402&q=${entry}&image_type=photo&pretty=true`)
+    console.log(response.data.hits)
+
+    this.setState({images: response.data.hits})
   }
 
   render(){
     return( 
       <div className="ui container" style={{marginTop:'30px'}}>
         <SearchInput onSearchSubmit={this.onSearchSubmit}/>
+        <ImageList images = {this.state.images} />
       </div>
       )
   }
