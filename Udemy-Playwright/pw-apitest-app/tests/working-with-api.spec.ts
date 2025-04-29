69821a8ebe60fc30d29ecf32bdc111ed5851518e
Dmitry <dmt.b@yahoo.com>
2025-04-29 11:07:01 -0400

    adding API performs

--- Udemy-Playwright/pw-apitest-app/tests/working-with-api.spec.ts
+++ Udemy-Playwright/pw-apitest-app/tests/working-with-api.spec.ts
@@ -14,3 +14,23 @@ test('has title', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
 });
 
+test('create the article', async({ page, request }) => {
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data:{
+      "user":{"email":"test-user2025@test.com", "password":"12345"}
+    }
+  })
+
+  const responseBody = await response.json()
+  const accessToken = responseBody.user.token
+
+  const articleresponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
+    data: {"title": "404", "description": "Hello 404", "body": "404 again", "tagList": ["tag-1"]},
+    headers: {
+      authorization: `Token ${accessToken}`
+    }
+  })
+
+  expect(articleresponse.status()).toEqual(201)
+})
+
