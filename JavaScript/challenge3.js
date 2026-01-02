95f5d42b34692c90c2152e14ab35a0e0807079c4
Dmitry-Boyko <dmt.b@yahoo.com>
2026-01-02 13:38:42 -0500

    JS challenge-3-update

--- JavaScript/challenge3.js
+++ JavaScript/challenge3.js
@@ -23,6 +23,6 @@ if (scoreDolphins > scoreKoalas) {
   console.log("Dolphins win the trophy");
 } else if (scoreKoalas > scoreDolphins) {
   console.log("Koalas win the trophy");
-} else ((scoreDolphins === scoreKoalas) ) {
+} else if (scoreDolphins === scoreKoalas) {
   console.log("Both win the trophy");
 }
