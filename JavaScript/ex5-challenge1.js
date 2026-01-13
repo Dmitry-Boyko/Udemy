b58fe2c252744f510e21613b66e62d13adf408ac
Dmitry <dmt.b@yahoo.com>
2026-01-13 15:46:52 -0500

    Create ex5-challenge1.js

---
+++ JavaScript/ex5-challenge1.js
@@ -0,0 +1,18 @@
+const calcAverage = (score1, score2, score3) => {
+    return (score1 + score2 + score3) / 3;
+}
+
+const scoreDolphins  = calcAverage(44, 23, 71)
+const scoreKoalas = calcAverage(65, 54, 49)
+
+const checkWinner = (scoreDolphins, scoreKoalas) => {
+    if (scoreKoalas >= 2 * scoreDolphins) {
+        console.log(`Koalas win ${scoreKoalas} vs ${scoreDolphins}`);
+    } else if (scoreKoalas < 2 * scoreDolphins) {
+            console.log(`Dolphins win ${scoreDolphins} vs ${scoreKoalas}`);
+    } else {
+        console.log("No team wins...")
+    }
+}
+
+checkWinner(scoreKoalas, scoreDolphins);
