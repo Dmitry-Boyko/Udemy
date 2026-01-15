847faf5e1eda422ead0ff98e6596fc0525775983
Dmitry-Boyko <dmt.b@yahoo.com>
2026-01-15 15:28:13 -0500

    arrays

--- complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -41,13 +41,63 @@
 
 // arrow function
 
-const calkAge3 = (birthYear) => 2037 - birthYear;
-const age3 = calkAge3(1911);
-console.log(age3);
+// const calkAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calkAge3(1991);
+// console.log(age3);
 
-const yaerUntilRetirement = (birthYear) => {
-  const age = 2026 - birthYear;
-  const retirement = 65 - age;
-  return retirement;
-};
-console.log(yaerUntilRetirement(1969));
+// const yaerUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   return `${firstName} retires is ${retirement}`;
+// };
+// console.log(yaerUntilRetirement(1989, "Bob"));
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangesPieces = cutFruitPieces(oranges);
+//   console.log(oranges, apples);
+//   const juice = `Juice with ${applePieces} piaces of apples and ${orangesPieces} piaces of oranges`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+// const calcAge = function (birthdYear) {
+//   return 2037 - birthdYear;
+// };
+
+// const yaerUntilRetirement = function (birthdYear, firstName) {
+//   const age = calcAge(birthdYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired 🍕`);
+//     return -1;
+//   }
+// };
+
+// console.log(yaerUntilRetirement(1991, "Bob"));
+// console.log(yaerUntilRetirement(1970, "Mike"));
+
+// Array
+const friends = ["Michael", "Tyler", "Joe", "Silvia"];
+
+console.log(friends.indexOf("Tyler"));
+
+for (let i = 3; i < 2; i++) {
+  const result = friends.shift();
+  console.log(`Removed: ${result}`);
+  console.log(`We're keeps only: ${friends}`);
+}
+
+for (let i = 0; i < 2; i++) {
+  const result = friends.pop();
+  console.log(`Removed: ${result}`);
+  console.log(`We're keeps only: ${friends}`);
+}
